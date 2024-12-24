9fb1620f2d00cf990c4e09fb100e9b50bc1396d4
Priyanka <priyanka.ss1@outlook.com>
2024-12-24 05:37:12 +0530

    chore: add get route

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 initializeDatabase();
+app.get('/', (req, res) => console.log('Hello, express'));
 
 async function createMovie(newMovie) {
   try {
